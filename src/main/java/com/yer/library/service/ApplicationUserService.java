c9dcd87bd49aab96ce2142bd288fcee7e708a7bb
Han Meerholz <hanmeerholz@gmail.com>
2022-11-11 17:01:54 +0100

    fixed a bug

--- src/main/java/com/yer/library/service/ApplicationUserService.java
+++ src/main/java/com/yer/library/service/ApplicationUserService.java
@@ -2,13 +2,14 @@ package com.yer.library.service;
 
 import com.yer.library.model.ApplicationUser;
 import com.yer.library.model.User;
-import org.mapstruct.ap.internal.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class ApplicationUserService implements UserDetailsService {
@@ -25,10 +26,11 @@ public class ApplicationUserService implements UserDetailsService {
                 () -> new UsernameNotFoundException("Username " + username + " not found")
         );
 
+
         return new ApplicationUser(
                 user.getUsername(),
                 user.getPassword(),
-                Collections.asSet(new SimpleGrantedAuthority("ROLE_ADMIN")),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")),
                 true,
                 true,
                 true,
